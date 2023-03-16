058f1f9d3b366ae8ded77cec2a4ca6e49ba4a7cf
Walter <trevor.wtc@gmail.com>
2023-03-16 04:55:08 -0700

    Hello World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<studio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        puts("with propergrammer, but the outcome is piece of art, \n");
+        return (0);
+}
